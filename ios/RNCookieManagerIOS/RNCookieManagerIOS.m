693510757ebf7fdab68993b9d67838612efc574c
Artem Serga <artem@serga.name>
2020-01-09 16:25:03 +0200

    Fix issue with expiresDate

--- ios/RNCookieManagerIOS/RNCookieManagerIOS.m
+++ ios/RNCookieManagerIOS/RNCookieManagerIOS.m
@@ -152,7 +152,10 @@ RCT_EXPORT_METHOD(
             [d setObject:c.name forKey:@"name"];
             [d setObject:c.domain forKey:@"domain"];
             [d setObject:c.path forKey:@"path"];
-            [d setObject:[self.formatter stringFromDate:c.expiresDate] forKey:@"expiresDate"];
+            NSString *expiresStr = [self.formatter stringFromDate:c.expiresDate];
+            if (expiresStr) {
+              [d setObject:expiresStr forKey:@"expiresDate"];
+            }
             [cookies setObject:d forKey:c.name];
         }
         resolve(cookies);
